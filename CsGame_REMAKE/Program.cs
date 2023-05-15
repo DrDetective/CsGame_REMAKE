5f122446cedfaf93d9780841d659721bebf3d7a9
Fon <bastici30@gmail.com>
2023-05-15 16:14:27 +0200

    ver 1.0.0

--- CsGame_REMAKE/Program.cs
+++ CsGame_REMAKE/Program.cs
@@ -6,7 +6,26 @@ List lists = new List();
 Player stats = new Player();
 Helper help = new Helper();
 #endregion ENTRY
-AnsiConsole.Write(new FigletText(" ").LeftJustified().Color(Color.Red));
+//??AnsiConsole.Write(new FigletText(" ").LeftJustified().Color(Color.Red));??
 AnsiConsole.Write(new FigletText("Wanderer's Tale").Centered().Color(Color.Red));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //https://www.geeksforgeeks.org/console-setwindowsize-method-in-c-sharp/
