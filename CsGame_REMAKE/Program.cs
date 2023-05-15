b0917f01334c4f319b8e9e18f3e533f0b1292137
Štěpán Bast <stepan.bast@educhem.cz>
2023-05-15 14:42:54 +0200

    ver 1.0.0

--- CsGame_REMAKE/Program.cs
+++ CsGame_REMAKE/Program.cs
@@ -8,3 +8,5 @@ Helper help = new Helper();
 #endregion ENTRY
 AnsiConsole.Write(new FigletText(" ").LeftJustified().Color(Color.Red));
 AnsiConsole.Write(new FigletText("Wanderer's Tale").Centered().Color(Color.Red));
+
+//https://www.geeksforgeeks.org/console-setwindowsize-method-in-c-sharp/
